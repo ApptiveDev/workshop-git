27d82c43f725ad8ea8c60b4c74225db637eab184
Cotidie <daily142857@gmail.com>
2023-08-07 10:07:08 +0900

    Change to use branch than fork on step 2

---
+++ lab/202308/calculator.py
@@ -0,0 +1,31 @@
+def add(a, b):
+    pass
+
+
+def subtract(a, b):
+    pass
+
+
+def multiply(a, b):
+    pass
+
+
+def divide(a, b):
+    pass
+
+
+def pow(a, b):
+    pass
+
+
+def abs(a):
+    pass
+
+
+def mod(a, b):
+    pass
+
+
+if __name__ == "__main__":
+    # 간단한 테스트 코드
+    pass
